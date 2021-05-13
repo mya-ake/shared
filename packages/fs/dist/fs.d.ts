baad860d02af3fe564e3a84b3eee0be0d36beb08
mya-ake <make.app.ts@gmail.com>
2021-05-14 08:05:24 +0900

    feat: add functions

--- packages/fs/dist/fs.d.ts
+++ packages/fs/dist/fs.d.ts
@@ -4,3 +4,9 @@ export declare const writeFile: (
   pathname: string,
   data: string,
 ) => Promise<void>;
+export declare const isDirectory: (pathname: string) => Promise<boolean>;
+export declare const extractFileName: (pathname: string) => string;
+export declare const extractExtension: (pathname: string) => string;
+export declare const getFilePathnames: (
+  dirPathname: string,
+) => Promise<string[]>;
--- packages/fs/src/fs.ts
+++ packages/fs/src/fs.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-const createDir = (pathname: string) => {
+const createDir = (pathname: string): Promise<void> => {
   return fs.promises.mkdir(pathname);
 };
 
-const existPathname = async (pathname: string) => {
+const existPathname = async (pathname: string): Promise<boolean> => {
   try {
     await fs.promises.stat(pathname);
     return true;
@@ -14,7 +14,7 @@ const existPathname = async (pathname: string) => {
   }
 };
 
-const ensureWriteProcess = async (pathname: string) => {
+const ensureWriteProcess = async (pathname: string): Promise<void> => {
   const fileDirname = path.dirname(pathname);
   if (await existPathname(fileDirname)) {
     return;
@@ -23,11 +23,44 @@ const ensureWriteProcess = async (pathname: string) => {
   await createDir(fileDirname);
 };
 
-export const readFile = (pathname: string) => {
+export const readFile = (pathname: string): Promise<Buffer> => {
   return fs.promises.readFile(pathname);
 };
 
-export const writeFile = async (pathname: string, data: string) => {
+export const writeFile = async (
+  pathname: string,
+  data: string,
+): Promise<void> => {
   await ensureWriteProcess(pathname);
   return fs.promises.writeFile(pathname, data, { encoding: 'utf8' });
+};
+
+export const isDirectory = async (pathname: string): Promise<boolean> => {
+  const pathState = await fs.promises.stat(pathname);
+  return pathState.isDirectory();
+};
+
+export const extractFileName = (pathname: string): string => {
+  return pathname.split('/').pop() || '';
+};
+
+export const extractExtension = (pathname: string): string => {
+  return pathname.split('.').pop() || '';
+};
+
+export const getFilePathnames = async (
+  dirPathname: string,
+): Promise<string[]> => {
+  const pathnames = await fs.promises.readdir(dirPathname);
+  let allPathnames: string[] = [];
+  for (const pathname of pathnames) {
+    const absoluteFilePathname = path.join(dirPathname, pathname);
+    if (await isDirectory(absoluteFilePathname)) {
+      const nestedFilePathnames = await getFilePathnames(absoluteFilePathname);
+      allPathnames = allPathnames.concat(nestedFilePathnames);
+    } else {
+      allPathnames = allPathnames.concat(absoluteFilePathname);
+    }
+  }
+  return allPathnames;
 };
