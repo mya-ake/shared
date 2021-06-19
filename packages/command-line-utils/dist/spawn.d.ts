023a1ab222ced356abf09164b60df1f6b1cc5b1a
mya-ake <make.app.ts@gmail.com>
2021-06-19 10:19:30 +0900

    refactor: export option type

--- packages/command-line-utils/dist/spawn.d.ts
+++ packages/command-line-utils/dist/spawn.d.ts
@@ -1,11 +1,12 @@
 /// <reference types="node" />
 import { ChildProcess } from 'child_process';
+export declare type SpawnOption = {
+  pararel?: boolean;
+};
 declare type Spawn = (
   command: string,
   args?: string[],
-  options?: {
-    pararel?: boolean;
-  },
+  option?: SpawnOption,
 ) => Promise<ChildProcess>;
 export declare const spawn: Spawn;
 export {};
